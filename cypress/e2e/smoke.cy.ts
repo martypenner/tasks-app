a26b4fbc2b9266c109d312f484786b9a1e938577
Marty Penner <martypenner@users.noreply.github.com>
2022-08-18 00:16:53 -0400

    tests: fix smoke tests

--- cypress/e2e/smoke.cy.ts
+++ cypress/e2e/smoke.cy.ts
@@ -15,18 +15,22 @@ describe('smoke tests', () => {
 
 		cy.visitAndCheck('/');
 
-		cy.findByRole('link', { name: /sign up/i }).click();
+		cy.findByRole('link', { name: /dashboard/i });
 
-		cy.findByRole('textbox', { name: /email/i }).type(loginForm.email);
-		cy.findByLabelText(/password/i).type(loginForm.password);
-		cy.findByRole('button', { name: /create account/i }).click();
+		// From template
+		// cy.findByRole('link', { name: /sign up/i }).click();
 
-		cy.findByRole('link', { name: /notes/i }).click();
-		cy.findByRole('button', { name: /logout/i }).click();
-		cy.findByRole('link', { name: /log in/i });
+		// cy.findByRole('textbox', { name: /email/i }).type(loginForm.email);
+		// cy.findByLabelText(/password/i).type(loginForm.password);
+		// cy.findByRole('button', { name: /create account/i }).click();
+
+		// cy.findByRole('link', { name: /notes/i }).click();
+		// cy.findByRole('button', { name: /logout/i }).click();
+		// cy.findByRole('link', { name: /log in/i });
 	});
 
-	it('should allow you to make a note', () => {
+	// From template
+	it.skip('should allow you to make a note', () => {
 		const testNote = {
 			title: faker.lorem.words(1),
 			body: faker.lorem.sentences(1),
