e936516915d87c915155f725f0d1a51203a78267
Marty Penner <martypenner@users.noreply.github.com>
2022-08-19 22:28:21 -0400

    tests: skip original tests for now

--- cypress/e2e/smoke.cy.ts
+++ cypress/e2e/smoke.cy.ts
@@ -5,7 +5,8 @@ describe('smoke tests', () => {
 		cy.cleanupUser();
 	});
 
-	it('should allow you to register and login', () => {
+	// From template
+	it.skip('should allow you to register and login', () => {
 		const loginForm = {
 			email: `${faker.internet.userName()}@example.com`,
 			password: faker.internet.password(),
@@ -15,18 +16,15 @@ describe('smoke tests', () => {
 
 		cy.visitAndCheck('/');
 
-		cy.findByRole('link', { name: /dashboard/i });
+		cy.findByRole('link', { name: /sign up/i }).click();
 
-		// From template
-		// cy.findByRole('link', { name: /sign up/i }).click();
+		cy.findByRole('textbox', { name: /email/i }).type(loginForm.email);
+		cy.findByLabelText(/password/i).type(loginForm.password);
+		cy.findByRole('button', { name: /create account/i }).click();
 
-		// cy.findByRole('textbox', { name: /email/i }).type(loginForm.email);
-		// cy.findByLabelText(/password/i).type(loginForm.password);
-		// cy.findByRole('button', { name: /create account/i }).click();
-
-		// cy.findByRole('link', { name: /notes/i }).click();
-		// cy.findByRole('button', { name: /logout/i }).click();
-		// cy.findByRole('link', { name: /log in/i });
+		cy.findByRole('link', { name: /notes/i }).click();
+		cy.findByRole('button', { name: /logout/i }).click();
+		cy.findByRole('link', { name: /log in/i });
 	});
 
 	// From template
