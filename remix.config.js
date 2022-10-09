8572544f8468c5889bfd47957d56cc6973451817
Marty Penner <martypenner@users.noreply.github.com>
2022-10-09 19:40:18 +0000

    fix: temporarily disable custom server

--- remix.config.js
+++ remix.config.js
@@ -6,5 +6,6 @@ module.exports = {
 	ignoredRouteFiles: ['**/.*', '**/*.css', '**/*.test.{js,jsx,ts,tsx}'],
 	// When running locally in development mode, we use the built in remix
 	// server. For the prod build, we want remix bundled as a sidecar for tauri.
-	server: process.env.NODE_ENV === 'development' ? undefined : './server.js',
+	// Note: temporarily disabled until remix figures itself out
+	// server: process.env.NODE_ENV === 'development' ? undefined : './server.js',
 };
