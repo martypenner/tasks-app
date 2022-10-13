3962758df8fdfca5ab3b1e317a97f0e66796e5b8
Marty Penner <martypenner@users.noreply.github.com>
2022-10-13 01:37:34 -0400

    chore: upgrade button to use react-aria

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -1,10 +1,20 @@
-export default function Button(props: React.ComponentPropsWithoutRef<'button'>) {
+import { useRef } from 'react';
+import type { AriaButtonProps } from 'react-aria';
+import { useButton } from 'react-aria';
+
+export default function Button(props: AriaButtonProps<'button'>) {
+	let ref = useRef(null);
+	let { buttonProps } = useButton(props, ref);
+
 	return (
 		<button
-			{...props}
+			{...buttonProps}
+			ref={ref}
 			className={`rounded bg-blue-500 py-2 px-4 text-white hover:bg-blue-600 focus:bg-blue-400 ${
+				// @ts-expect-error
 				props.className ?? ''
 			}`}
+			{...props}
 		/>
 	);
 }
